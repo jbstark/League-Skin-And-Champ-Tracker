5ae581af227dad6ede68f5bbd044fa69fb8eb1d6
george <george@mabbutts.com>
2021-05-24 23:49:01 -0600

    added scroll area to tab_widget, removed api/username section, added stats section to display champs owned/be needed, implemented refresh button

--- LeagueTrackerGUI.py
+++ LeagueTrackerGUI.py
@@ -1,4 +1,6 @@
 from QtGUI import Ui_MainWindow
+from Client import Client
+from flowlayout import FlowLayout
 from PyQt5 import QtWidgets, QtCore
 
 
@@ -9,6 +11,18 @@ class TrackerWindow(QtWidgets.QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.client = Client()
+        self.refresh()
+
+        self.ui.refresh_button.clicked.connect(self.refresh)
+
+    def refresh(self):
+        self.client.update()
+        num1 = self.client.get_num_champs(True)
+        num2 = self.client.get_ip_needed("max", True)
+        self.ui.num_champs_owned_value_label.setText(f'{num1:,}')
+        self.ui.max_blue_essence_needed_value_label.setText(f'{num2:,}')
 
 
 if __name__ == "__main__":
