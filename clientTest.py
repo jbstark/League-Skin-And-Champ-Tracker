de053237b3bdf9fc2a5bbde0056fc14be507c400
Jeremy Stark <jbstark@umich.edu>
2021-05-26 16:24:00 -0700

    Updated tests to reflect change in get_ip_needed

--- clientTest.py
+++ clientTest.py
@@ -12,16 +12,18 @@ print(client.get_champs(False))
 print("Num Unowned")
 print(client.get_num_champs(False))
 
-print("IP Max:", client.get_ip_needed("max", False))
-print("IP Max minus owned:", client.get_ip_needed("max", True))
+print("IP Max:", client.get_ip_needed("max", False, True))
+print("IP Max minus owned:", client.get_ip_needed("max", True, True))
 
-print("IP Min:", client.get_ip_needed("min", False))
-print("IP Min minus owned:", client.get_ip_needed("min", True))
+print("IP Min:", client.get_ip_needed("min", False, True))
+print("IP Min minus owned:", client.get_ip_needed("min", True, True))
 
-print("IP Current:", client.get_ip_needed("current", False))
-print("IP Current minus owned:", client.get_ip_needed("current", True))
+print("IP Current:", client.get_ip_needed("current", False, True))
+print("IP Current minus owned:", client.get_ip_needed("current", True, True))
 
 print("All Data")
 client.print_all_data()
 
 client.get_sorted_champs("lastPlayed", "desc", "name", "asc", True)
+
+print(client.call_api('/lol-game-data/assets/v1/champion-icons'))
